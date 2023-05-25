f3987530db6147ba423e56c42aa4c5959b2cb3a2
Stefan Olivier <s.olivier1194@gmail.com>
2023-05-25 14:48:14 +0200

    removed adhoc ArgumentParser from examples

--- examples/timecode_basic-increment.example.cpp
+++ examples/timecode_basic-increment.example.cpp
@@ -4,33 +4,15 @@
 #include <string_view>
 #include <cstring>
 #include "timecode.hpp"
-#include "common.hpp"
 
 int main(int argc, char** argv)
 {
-    constexpr auto arg1 = ArgumentParserBuilder::new_argument("timecode1", "string", "positional");
-    constexpr auto arg2 = ArgumentParserBuilder::new_argument("timecode2", "string", "positional");
-
-    constexpr auto config = ArgumentParserBuilder::new_configuration("cxxtc timecode example: increment",
-                                                                     "simple toy program that demonstrates incrementing a cxxtc timecode by another timecode.",
-                                                                     arg1, arg2);
-
-    constexpr auto parser = ArgumentParserBuilder::new_parser(config);
-
-    try {
-        const auto parsed_arguments = parser(argc, argv);
-        parsed_arguments.print_usage();
-    } catch (const std::exception& error) {
-        std::cout << "error: " << error.what() << '\n';
-        std::exit(EXIT_FAILURE);
+    cxxtc::timecode tc2 = 0;
+    for (std::size_t i = 0; i < 3601; ++i) {
+        auto fps = cxxtc::fps::to_unsigned(tc2.fps());
+        std::cout << "value: " << tc2 << " fps: " << fps << '\n';
+        tc2 += (100 * fps);
     }
-
-    // cxxtc::timecode tc2 = 0;
-    // for (std::size_t i = 0; i < 3601; ++i) {
-    //     auto fps = cxxtc::fps::to_unsigned(tc2.fps());
-    //     std::cout << "value: " << tc2 << " fps: " << fps << '\n';
-    //     tc2 += (100 * fps);
-    // }
 
     return 0;
 }
