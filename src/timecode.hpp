ecfad3e4147f75b6749a51ca47d3fc86d5ad3eac
Stefan Olivier <s.olivier1194@gmail.com>
2024-08-24 20:27:15 +0200

    implement timecode string constructor

--- src/timecode.hpp
+++ src/timecode.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <cassert>
+#include <compare>
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
@@ -11,6 +12,9 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <stdexcept>
+#include <format>
+#include <iostream>
 
 // -----------------------------------------------------------------------------
 //
@@ -20,34 +24,48 @@
 
 // Logging
 #define CXXTC_TODO(msg) (assert(0 && msg))
+#define CXXTC_ASSERT(assertion) (assert(assertion))
+#define CXXTC_THROW(msg) throw std::runtime_error((msg))
 
 // Class Macro Helpers
 #define LITERAL(symbol) #symbol
 #define DELETE_CTORS(type)                 \
     type() = delete;                       \
-    type(const type&) = delete;            \
-    type& operator=(const type&) = delete; \
+    type operator=(type const&) = delete;  \
     type(type&&) = delete;                 \
     type& operator=(type&&) = delete;
 
 // For Enums
 #define ENUM_VARIANTS(...) {__VA_ARGS__}
 #define ENUM_BODY(...) __VA_ARGS__
+#define ENUM_THREE_WAY_OPERATOR(type, me)                                                 \
+    template<std::same_as<type> T>                                                        \
+    auto operator<=>(const T& other) const noexcept {                                     \
+    return (me < other._variant || me > other._variant)                                   \
+               ? (me < other._variant)                                                    \
+                   ? std::strong_ordering::less                                           \
+                   : std::strong_ordering::greater                                        \
+           : std::strong_ordering::equal;                                                 \
+}                                                                                         \
+    template<std::same_as<type> T>                                                        \
+    friend constexpr bool operator==(T lhs, T rhs) noexcept { return lhs <=> rhs == 0; };
 
-#define DECLARE_ENUM(type, underlying, variants, ...)                                                      \
-    struct type {                                                                                          \
-        using underlying_type = underlying;                                                                \
-        static constexpr const char* type_name = LITERAL(type);                                            \
-        enum Variant : underlying_type variants;                                                           \
-        __VA_ARGS__                                                                                        \
-        type(Variant variant) : _variant(variant) {}                                                       \
-        void operator=(Variant variant)  { _variant = variant; }                                           \
-        auto operator<=>(const type&) const = default;                                                     \
-        constexpr underlying_type as_underlying() const { return static_cast<underlying_type>(_variant); } \
-        constexpr Variant as_variant() const { return _variant; }                                          \
-        DELETE_CTORS(type)                                                                                 \
-        private:                                                                                           \
-            Variant _variant;                                                                              \
+#define DECLARE_ENUM(type, underlying, variants, ...)                                                             \
+    struct type {                                                                                                 \
+        using underlying_type = underlying;                                                                       \
+        static constexpr const char* type_name = LITERAL(type);                                                   \
+        enum Variant : underlying_type variants;                                                                  \
+        __VA_ARGS__                                                                                               \
+        type(Variant variant) : _variant(variant) {}                                                              \
+        type(type const& other) : _variant(other._variant) {}                                                     \
+        ENUM_THREE_WAY_OPERATOR(type, _variant)                                                                   \
+        inline constexpr void operator=(Variant variant)  { _variant = variant; }                                 \
+        inline constexpr underlying_type as_underlying() const { return static_cast<underlying_type>(_variant); } \
+        inline constexpr Variant as_variant() const { return _variant; }                                          \
+        inline constexpr operator Variant() const { return _variant; }                                            \
+        DELETE_CTORS(type)                                                                                        \
+        private:                                                                                                  \
+            Variant _variant;                                                                                     \
     }
 
 // -----------------------------------------------------------------------------
@@ -73,9 +91,24 @@ namespace __cxxtc {
         ),
 
         ENUM_BODY(
-            template<std::floating_point F>
-            constexpr Fps(F value) {
-                CXXTC_TODO("not implemented");
+            // TODO: handle truncation of fractional values
+            template<std::unsigned_integral T>
+            static constexpr T to_unsigned(Fps fps) {
+                 switch (fps) {
+                    case F_23P976_DF:
+                    case F_23P976_NDF: return 24;
+
+                    case F_24: return 24;
+
+                    case F_25: return 25;
+
+                    case F_29P97_DF:
+                    case F_29P97_NDF:
+                    case F_30: return 30;
+
+                    // TODO: formatter for enums
+                    default: CXXTC_THROW(std::format("unknown fps type with value: {}", fps.as_underlying()));
+                 }
             }
         )
     );
@@ -96,13 +129,27 @@ namespace __cxxtc {
 #define CXXTC_TICK_RATE_DEFAULT 1000
 #define CXXTC_TICKS_DEFAULT 0
 #define CXXTC_FLAGS_DEFAULT 0
+#define CXXTC_REGULAR_FORM_SIZE 11
+#define CXXTC_EXTENDED_FORM_SIZE 15
+#define CXXTC_HRS_BEGIN_INDEX 0
+#define CXXTC_MINS_BEGIN_INDEX 3
+#define CXXTC_SECS_BEGIN_INDEX 6
+#define CXXTC_FRAMES_BEGIN_INDEX 9
+#define CXXTC_TICKS_BEGIN_INDEX 12
+#define CXXTC_HRS_MAX 24
+#define CXXTC_MINS_MAX 59
+#define CXXTC_SECS_MAX 59
+#define CXXTC_1HR_TICKS(fps, ticks) (60 * 60 * (fps) * (ticks))
+#define CXXTC_1MIN_TICKS(fps, ticks) (60 * (fps) * (ticks))
+#define CXXTC_1SEC_TICKS(fps, ticks) ((fps) * (ticks))
+#define CXXTC_1FRAME_TICKS(ticks) ((ticks))
 
 // TODO: static interface assertion with concept
 template<std::unsigned_integral IntType>
 struct BasicTimecode {
     using fps_enum_type = __cxxtc::Fps;
     using fps_variant_type = __cxxtc::Fps::Variant;
-    using fps_type = fps_variant_type;
+    using fps_type = fps_enum_type;
     using ticks_type = IntType;
     using flags_type = std::uint8_t;
     using string_type = std::string;
@@ -132,7 +179,85 @@ struct BasicTimecode {
         , _ticks(CXXTC_TICKS_DEFAULT)
         , _flags(CXXTC_FLAGS_DEFAULT)
     {
-        CXXTC_TODO("not implemented");
+        auto const ticks = BasicTimecode::timecode_to_ticks(tc, fps);
+        if (!ticks.has_value()) {
+            CXXTC_THROW(std::format("failed to construct timecode from string \"{}\" with fps value \"{}\"", tc, fps.as_underlying()));
+        }
+        _ticks = ticks.value();
+    }
+
+    static constexpr std::optional<ticks_type> timecode_to_ticks(std::string_view tc, fps_type fps) noexcept {
+        auto const tc_size = tc.size();
+        if (tc_size != CXXTC_REGULAR_FORM_SIZE && tc_size != CXXTC_EXTENDED_FORM_SIZE) {
+            return std::nullopt;
+        }
+
+        ticks_type ticks = 0;
+        auto const fps_unsigned = fps_enum_type::to_unsigned<ticks_type>(fps);
+
+        for (std::size_t i = 0; i < tc_size; i += 3) {
+            auto const first_char = tc[i + 0];
+            auto const second_char = tc[i + 1];
+
+            // NOTE: In the regular form, size of tc_string is NOT a multiple
+            // of 3. We do not assume that the user has provided us with a
+            // valid null-terminated string, therefore, we explicitly terminate
+            // with a null-byte.
+            auto const third_char = (tc_size == CXXTC_EXTENDED_FORM_SIZE || i < CXXTC_FRAMES_BEGIN_INDEX)
+                ? tc[i + 2]
+                : '\0';
+
+            if (!('0' <= first_char && first_char <= '9') && !('0' <= second_char && second_char <= '9')) {
+                return std::nullopt;
+            }
+
+            auto const last_is_not_number = !('0' <= third_char && third_char <= '9');
+            auto const last_is_not_delimiter = (i == CXXTC_SECS_BEGIN_INDEX)
+                ? (third_char != ':' && third_char != ';')
+                : (i == CXXTC_FRAMES_BEGIN_INDEX)
+                    ? (tc_size == CXXTC_EXTENDED_FORM_SIZE)
+                        ? (third_char != '.')
+                        : (third_char != '\0')
+                    : third_char != ':';
+
+            if ((i == CXXTC_TICKS_BEGIN_INDEX) ? last_is_not_number : last_is_not_delimiter) { return std::nullopt; }
+
+            auto const hundreds = (first_char - '0') * 100u;
+            auto const tens = (i == CXXTC_TICKS_BEGIN_INDEX) ? (second_char - '0') * 10u : (first_char - '0') * 10u;
+            auto const units = (i == CXXTC_TICKS_BEGIN_INDEX) ? (third_char - '0') * 1u : (second_char - '0') * 1u;
+            auto const value = (i == CXXTC_TICKS_BEGIN_INDEX) ? hundreds + tens + units : tens + units;
+
+            switch (i) {
+                case CXXTC_HRS_BEGIN_INDEX: {
+                    if (value > CXXTC_HRS_MAX) { return std::nullopt; }
+                    ticks += value * CXXTC_1HR_TICKS(fps_unsigned, TICK_RATE);
+                } break;
+
+                case CXXTC_MINS_BEGIN_INDEX: {
+                    if (value > CXXTC_MINS_MAX) { return std::nullopt; }
+                    ticks += value * CXXTC_1MIN_TICKS(fps_unsigned, TICK_RATE);
+                } break;
+
+                case CXXTC_SECS_BEGIN_INDEX: {
+                    if (value > CXXTC_SECS_MAX) { return std::nullopt; }
+                    ticks += value * CXXTC_1SEC_TICKS(fps_unsigned, TICK_RATE);
+                } break;
+
+                case CXXTC_FRAMES_BEGIN_INDEX: {
+                    if (value >= fps_unsigned) { return std::nullopt; }
+                    ticks += value * CXXTC_1FRAME_TICKS(TICK_RATE);
+                } break;
+
+                case CXXTC_TICKS_BEGIN_INDEX: {
+                    if (value >= TICK_RATE) { return std::nullopt; }
+                    ticks += value;
+                } break;
+
+                // unreachable
+                default: return std::nullopt;
+            }
+        }
+        return ticks;
     }
 
     template<std::unsigned_integral T>
@@ -239,6 +364,44 @@ struct BasicTimecode {
         CXXTC_TODO("not implemented");
     }
 
+    constexpr ticks_type hours_part() const {
+        return _ticks / CXXTC_1HR_TICKS(fps_enum_type::to_unsigned<ticks_type>(_fps), TICK_RATE);
+    }
+
+    constexpr ticks_type minutes_part() const {
+        auto const fps_unsigned = fps_enum_type::to_unsigned<ticks_type>(_fps);
+        auto const reduced = _ticks % CXXTC_1HR_TICKS(fps_unsigned, TICK_RATE);
+        return reduced / CXXTC_1MIN_TICKS(fps_unsigned, TICK_RATE);
+    }
+
+    constexpr ticks_type seconds_part() const {
+        auto const fps_unsigned = fps_enum_type::to_unsigned<ticks_type>(_fps);
+        auto const reduced = _ticks % CXXTC_1MIN_TICKS(fps_unsigned, TICK_RATE);
+        return reduced / CXXTC_1SEC_TICKS(fps_unsigned, TICK_RATE);
+    }
+
+    constexpr ticks_type frames_part() const {
+        auto const reduced = _ticks % CXXTC_1SEC_TICKS(fps_enum_type::to_unsigned<ticks_type>(_fps), TICK_RATE);
+        return reduced / CXXTC_1FRAME_TICKS(TICK_RATE);
+    }
+
+    constexpr ticks_type ticks_part() const {
+        auto const reduced = _ticks % CXXTC_1FRAME_TICKS(TICK_RATE);
+        return reduced;
+    }
+
+    inline constexpr fps_type fps() const {
+        return _fps;
+    }
+
+    inline constexpr ticks_type ticks() const {
+        return _ticks;
+    }
+
+    inline constexpr flags_type flags() const {
+        return _flags;
+    }
+
 private:
     fps_type _fps;
     ticks_type _ticks;
@@ -261,6 +424,20 @@ private:
 #undef CXXTC_TODO
 #undef CXXTC_TICKS_DEFAULT
 #undef CXXTC_FLAGS_DEFAULT
+#undef CXXTC_REGULAR_FORM_SIZE
+#undef CXXTC_EXTENDED_FORM_SIZE
+#undef CXXTC_HRS_BEGIN_INDEX
+#undef CXXTC_MINS_BEGIN_INDEX
+#undef CXXTC_SECS_BEGIN_INDEX
+#undef CXXTC_FRAMES_BEGIN_INDEX
+#undef CXXTC_TICKS_BEGIN_INDEX
+#undef CXXTC_HRS_MAX
+#undef CXXTC_MINS_MAX
+#undef CXXTC_SECS_MAX
+#undef CXXTC_1HR_TICKS
+#undef CXXTC_1MIN_TICKS
+#undef CXXTC_1SEC_TICKS
+#undef CXXTC_1FRAME_TICKS
 
 // -----------------------------------------------------------------------------
 
--- tests/timecode.test.cpp
+++ tests/timecode.test.cpp
@@ -1,11 +1,92 @@
-#include "timecode.hpp"
 #include "test.hpp"
+#include "timecode.hpp"
 
 SUITE("timecode") {
-    SECTION("initialization") {
-        TEST("constructible from fps with static member") {
-            __cxxtc::BasicTimecode<std::uint32_t> tc1{ __cxxtc::Fps::F_25 };
-            ASSERT(tc1.TICK_RATE == 1000);
+    using enum __cxxtc::Fps::Variant;
+    using namespace __cxxtc;
+    using Timecode = BasicTimecode<std::uint32_t>;
+    auto constexpr TICK_RATE = Timecode::TICK_RATE;
+
+    SECTION("conversion to and from string") {
+        TEST("conversion from valid tc strings succeed") {
+            auto const tc_string_regular = "00:01:42:12";
+            auto const tc_string_extended = "00:01:42:12.690";
+
+            auto const expected_ticks_regular =
+                (0 * 60 * 60 * 25 * TICK_RATE)
+                + (1 * 60 * 25 * TICK_RATE)
+                + (42 * 25 * TICK_RATE)
+                + (12 * TICK_RATE)
+                + 0;
+
+            auto const expected_ticks_extended =
+                (0 * 60 * 60 * 25 * TICK_RATE)
+                + (1 * 60 * 25 * TICK_RATE)
+                + (42 * 25 * TICK_RATE)
+                + (12 * TICK_RATE)
+                + (690);
+
+            auto const ticks_regular = Timecode::timecode_to_ticks(tc_string_regular, F_25).value();
+            ASSERT(ticks_regular == expected_ticks_regular);
+
+            auto const ticks_extended = Timecode::timecode_to_ticks(tc_string_extended, F_25).value();
+            ASSERT(ticks_extended == expected_ticks_extended);
+        };
+
+        TEST("conversion from invalid tc strings fail") {
+            auto const ticks_empty_string = Timecode::timecode_to_ticks("", F_25);
+            ASSERT(!ticks_empty_string.has_value() == true);
+
+            auto const ticks_bad_delimiter = Timecode::timecode_to_ticks("01:02:03.04", F_25);
+            ASSERT(!ticks_bad_delimiter.has_value() == true);
+
+            auto const ticks_bad_hrs = Timecode::timecode_to_ticks("25:02:03:01", F_25);
+            ASSERT(!ticks_bad_hrs.has_value() == true);
+
+            auto const ticks_bad_mins = Timecode::timecode_to_ticks("01:72:03:10", F_25);
+            ASSERT(!ticks_bad_mins.has_value() == true);
+
+            auto const ticks_bad_secs = Timecode::timecode_to_ticks("01:02:69:24", F_25);
+            ASSERT(!ticks_bad_secs.has_value() == true);
+
+            auto const ticks_bad_fps = Timecode::timecode_to_ticks("01:02:03:25", F_25);
+            ASSERT(!ticks_bad_fps.has_value() == true);
+
+            auto const ticks_bad_ticks = Timecode::timecode_to_ticks("01:02:03:00.1000", F_25);
+            ASSERT(!ticks_bad_ticks.has_value() == true);
+
+            auto const no_leading_zeroes = Timecode::timecode_to_ticks("01:2:03:0.0", F_25);
+            ASSERT(!no_leading_zeroes.has_value() == true);
+
+            auto const letters_in_string = Timecode::timecode_to_ticks("de:ad:be:ef", F_25);
+            ASSERT(!letters_in_string.has_value() == true);
+        };
+    };
+
+    SECTION("internals yield expected values") {
+        TEST("expected defaults") {
+            Timecode tc1{ F_25 };
+            ASSERT(tc1.ticks() == 0);
+            ASSERT(tc1.fps() == F_25);
+            ASSERT(tc1.flags() == 0);
+        };
+
+        TEST("expected parts from timecode string") {
+            Timecode tc1{ "00:01:42:12.690", F_25 };
+            ASSERT(tc1.hours_part() == 0);
+            ASSERT(tc1.minutes_part() == 1);
+            ASSERT(tc1.seconds_part() == 42);
+            ASSERT(tc1.frames_part() == 12);
+            ASSERT(tc1.ticks_part() == 690);
+
+            auto const ticks_sanity_check =
+                (0 * 60 * 60 * 25 * TICK_RATE)
+                + (1 * 60 * 25 * TICK_RATE)
+                + (42 * 25 * TICK_RATE)
+                + (12 * TICK_RATE)
+                + (690);
+                
+            ASSERT(tc1.ticks() == ticks_sanity_check);
         };
     };
 }
